2355ce782cf13cc241a5d02cb5c4bf6430a3564f
ayeshaali <aliayeshanyc@gmail.com>
2018-09-29 19:38:06 -0400

    message

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,7 @@
 var faveQuotes = [
     '"Put your heart, mind, and soul into even your smallest acts. This is the secret of success."<br> -Swami Sivananda',
     '"Strength is a matter of the made-up mind."<br> -John Beecher',
-    '"Anything is possible if you\'ve got enough nerve"<br> -JK Rowling',
+    '"Anything is possible if you\'ve got enough nerve."<br> -JK Rowling',
     '“Think lightly of yourself and deeply of the world.”<br> -Miyamoto Musashi',
     '"We gain strength, and courage, and confidence by each experience in which we really stop to look fear in the face."<br> -Eleanor Roosevelt',
     '"In a gentle way, you can shake the world."<br> -Mahatma Ghandi',
